28596a75032933f44b2b07c5c0e0c922245b5e4e
JaewookMun <mun1103.dev@gmail.com>
2021-10-29 23:20:15 +0900

    연습문제 - 반복문

---
+++ src/com/book/algorithm/basics/Loop.java
@@ -0,0 +1,47 @@
+package com.book.algorithm.basics;
+
+public class Loop {
+
+    // Q7. sum 1~n : n=7
+    public static int sum(int n){
+        int sum=0;
+        for(int i=1; i<=n; i++){
+            sum+=i;
+        }
+
+        return sum;
+    }
+
+    // Q8. 가우스 덧셈
+    public static int gauthSum(int n){
+        int sum=0;
+
+        for(int i=1; i<=(n/2); i++){
+            sum+=(i+(n-(i-1)));
+        }
+
+        if(n%2!=0) sum+=(n/2)+1;
+
+        return sum;
+    }
+
+    // Q9. a~b 범위의 모든 정수의 합
+    public static int rangeSum(int a, int b){
+        int sum=0;
+
+        for(int i=a; i<=b; i++){
+            sum+=i;
+        }
+
+        return sum;
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(sum(7));
+        System.out.println(gauthSum(10));
+        System.out.println(rangeSum(3,5));
+    }
+
+}
