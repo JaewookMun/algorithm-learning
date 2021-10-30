cf8e421cade7f69d9b42c514bcc098178a857b2c
JaewookMun <mun1103.dev@gmail.com>
2021-10-30 23:58:46 +0900

    출력디자인 변경

--- src/com/book/algorithm/basics/MultiLoopMultiple.java
+++ src/com/book/algorithm/basics/MultiLoopMultiple.java
@@ -6,13 +6,17 @@ public class MultiLoopMultiple {
         for(int i=0; i<=9; i++) {
             for(int j=0; j<=9; j++) {
                 if(i==0 && j==0){
-                    System.out.print(" |"); continue;
+                    System.out.printf("%4s", "|"); continue;
                 } else if(i==0){
-                    System.out.print(j); continue;
+                    System.out.printf("%3d",j);
+                    if(j==9){
+                        System.out.print("\n---+---------------------------");
+                    }
+                    continue;
                 } else if(j==0){
-                    System.out.print(i+"|"); continue;
+                    System.out.printf("%3d|",i); continue;
                 }
-                System.out.print(i*j);
+                System.out.printf("%3d",i*j);
             }
             System.out.print("\n");
         }
