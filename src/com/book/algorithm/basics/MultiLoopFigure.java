aac7a63bfe6a8b1de1b5908eaf3231a2764882d7
JaewookMun <mun1103.dev@gmail.com>
2021-10-31 22:14:47 +0900

    Q16. n단 피라미드 출력 메소드

---
+++ src/com/book/algorithm/basics/MultiLoopFigure.java
@@ -0,0 +1,77 @@
+package com.book.algorithm.basics;
+
+public class MultiLoopFigure {
+    // Q15. 직각 이등변 삼각형 - 왼쪽아래 직각
+    public static void triangleLB(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    // 왼쪽 위 직각
+    public static void triangleLU(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    // 오른쪽 위 직각
+    public static void triangleRU(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i>j){
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+    // 오른쪽 아래 직각
+    public static void triangleRB(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i+j<n-1){
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // Q16. n단 피라미드 출력
+    public static void spira(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<2*n-1; j++){
+                if(j<n-(i+1) || j>=(n-(i+1))+2*(i+1)-1){
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        triangleLB(3);
+        System.out.println();
+        triangleLU(3);
+        System.out.println();
+        triangleRU(3);
+        System.out.println();
+        triangleRB(3);
+        System.out.println();
+
+        spira(4);
+        spira(10);
+    }
+}
