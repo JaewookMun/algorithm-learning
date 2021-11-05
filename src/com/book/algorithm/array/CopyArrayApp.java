60c9cb4d2c31c7cafb14ecafce70ed6f6b8f604e
JaewookMun <mun1103.dev@gmail.com>
2021-11-05 23:57:39 +0900

    Q4., Q5. - 배열복사

---
+++ src/com/book/algorithm/array/CopyArrayApp.java
@@ -0,0 +1,17 @@
+package com.book.algorithm.array;
+
+public class CopyArrayApp {
+    // Q4. 배열 b의 모든 요소를 배열 a에 복사하는 메서드 작성
+    public static void copy(int[] a, int[] b){
+        for(int i=0; i<b.length; i++){
+            a[i]=b[i];
+        }
+    }
+
+    // Q5. 배열 b의 모든 요소를 배열 a에 역순으로 복사하는 메서드
+    public static void rCopy(int[] a, int[] b){
+        for(int i=0; i<b.length; i++){
+            a[i]=b[b.length-1-i];
+        }
+    }
+}
