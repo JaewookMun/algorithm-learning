aa0bca306469090baa8bd303d74047eff70d0e6b
JaewookMun <mun1103.dev@gmail.com>
2021-11-09 23:20:32 +0900

    Q1. 선형검색 연습문제

---
+++ src/com/book/algorithm/search/SeqSearchSen.java
@@ -0,0 +1,10 @@
+package com.book.algorithm.search;
+
+// Q1. 선형검색 - 맨 처음부터 값을 비교해보는 검색알고리즘
+public class SeqSearchSen {
+
+    public static int seqSearchSen(int[] a, int n, int key){
+
+        return 0;
+    }
+}
