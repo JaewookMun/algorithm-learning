e8d6ab9d21d896df83f33b44f07befc6a9b3318a
JaewookMun <mun1103.dev@gmail.com>
2021-11-11 23:48:52 +0900

    Q2. 선형(순차)검색 스캐닝 과정 출력

--- src/com/book/algorithm/search/SeqSearchScanning.java
+++ src/com/book/algorithm/search/SeqSearchScanning.java
@@ -2,12 +2,44 @@ package com.book.algorithm.search;
 
 public class SeqSearchScanning {
 
-    public static void printScanningArray(int[] array){
-        System.out.printf("%10d", 10);
+    public static void printScanningArray(int[] array, int x){
+        System.out.printf("%2s|", "");
+        for(int i=0; i<array.length; i++){
+            System.out.printf("%2d", i);
+        }
+        System.out.println();
+
+        System.out.print("--+");
+        for(int i=0; i<array.length; i++){
+            System.out.printf("%s", "--");
+        }
+        System.out.println();
+
+
+        //System.out.printf("%s|", "");
+        for(int i=0; i<array.length; i++){
+            System.out.print("  |");
+            for(int j=0; j<i; j++){
+                System.out.printf("  ");
+            }
+            System.out.printf("%2s", "*");
+
+            System.out.println();
+            System.out.printf("%2d|", i);
+            for(int j=0; j<array.length; j++){
+                System.out.printf("%2d", array[j]);
+            }
+            System.out.println();
+
+            if(array[i]==x){
+                System.out.println(x+"는(은) index ["+i+"]에 있습니다.");
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
-        int[] test={};
-        printScanningArray(test);
+        int[] test={6,4,3,2,1,9,8};
+        printScanningArray(test, 3);
     }
 }
