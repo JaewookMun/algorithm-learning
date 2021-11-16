19230f079143f5b450d5469cf51b187c1c33148d
JaewookMun <mun1103.dev@gmail.com>
2021-11-16 23:38:44 +0900

    Q3. 문제 작성

---
+++ src/com/book/algorithm/search/BinSearchApp.java
@@ -0,0 +1,16 @@
+package com.book.algorithm.search;
+
+public class BinSearchApp {
+
+    // Q3. 요솟수가 n인 배열 a에서 key와 일치하는 모든 요소의 인덱스를 배열 idx에 순서대로 저장하고, 일치하는 요솟수를 반환하는 메서드
+    public static int searchIdx(int[] a, int n, int key, int[] idx){
+
+        int number=0;
+
+        for(int i=0; i<a.length; i++){
+
+        }
+
+        return number;
+    }
+}
