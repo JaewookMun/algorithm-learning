e46d9f82fa645b0f6a0135e9245f61adebae641f
JaewookMun <mun1103.dev@gmail.com>
2021-11-18 23:43:35 +0900

    Q4. 이진검색과정 출력

---
+++ src/com/book/algorithm/search/BinSearchPrinterApp.java
@@ -0,0 +1,62 @@
+package com.book.algorithm.search;
+
+import java.util.Optional;
+
+public class BinSearchPrinterApp {
+
+    // Q4. 이진검색 과정 출력 프로그램
+    public static void printBinSearching(int[] array, int target){
+        int medium=array.length/2;
+
+        // first line
+        System.out.printf("%2s|", "");
+        for(int i=0; i<array.length; i++){
+            System.out.printf("%3d", i);
+        }
+        System.out.println();
+
+        // second line
+        System.out.print("--+");
+        for(int i=0; i<array.length; i++){
+            System.out.printf("%s", "---");
+        }
+        System.out.println();
+
+        for(int k=0; k<array.length/2; k++){
+            // first searching
+            System.out.print("  |");
+            System.out.print(" <-");
+            for(int j=0; j<medium-1; j++){
+                System.out.print("   ");
+            }
+            System.out.print("  +");
+            for(int j=0; j<medium-1; j++){
+                System.out.print("   ");
+            }
+            System.out.print(" ->");
+            System.out.println();
+
+            System.out.printf("%2d|", medium);
+            for(int i=0; i<array.length; i++){
+                System.out.printf("%3d", array[i]);
+            }
+            System.out.println();
+
+            if(array[medium]==target){
+                System.out.println(target+"는 ["+medium+"]에 있습니다.");
+                break;
+            }
+            medium/=2;
+        }
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] temp={1,2,3,5,6,8,9};
+
+        printBinSearching(temp, 2);
+    }
+}
