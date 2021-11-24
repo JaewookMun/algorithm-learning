5f663dd32c54bfa78bc41d18bbcfa01fb7f866b6
JaewookMun <mun1103.dev@gmail.com>
2021-11-24 22:49:39 +0900

    ...

--- src/com/book/algorithm/search/PhysExamSearchEx.java
+++ src/com/book/algorithm/search/PhysExamSearchEx.java
@@ -1,11 +1,12 @@
 package com.book.algorithm.search;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 // Q7. 시력에 대한 내림차순 정렬에서 특정 시력을 가진 사람을 검색
 public class PhysExamSearchEx {
 
-    class PhyscData{
+    static class PhyscData{
         private String name;
         private int height;
         private double vision;
@@ -27,14 +28,28 @@ public class PhysExamSearchEx {
         // comparator based on eye
         public static final Comparator<PhyscData> EYESIGHT_ORDER = new EyesightOrderComparator();
 
-        private class EyesightOrderComparator implements Comparator<PhyscData>{
+        private static class EyesightOrderComparator implements Comparator<PhyscData>{
 
             @Override
             public int compare(PhyscData o1, PhyscData o2) {
-                return 0;
+
+                return (o1.vision > o2.vision) ? 1 : (o1.vision < o2.vision) ? -1 : 0;
             }
         }
     }
 
+    public static void main(String[] args) {
+        PhyscData[] x = {
+            new PhyscData("James", 162, 0.3),
+            new PhyscData("Keven", 180, 0.5),
+            new PhyscData("Robert", 175, 1.3),
+            new PhyscData("Kim", 178, 0.8),
+            new PhyscData("Yuna", 154, 0.9),
+            new PhyscData("Gert", 183, 1.2),
+        };
 
+        int idx = Arrays.binarySearch(x, new PhyscData("", 0, 1.2), PhyscData.EYESIGHT_ORDER);
+        System.out.println(idx);
+
+    }
 }
