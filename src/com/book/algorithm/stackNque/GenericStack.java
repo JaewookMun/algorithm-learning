ad213eac2b93cc5269afc451cc2d0e5536f1ea83
JaewookMun <mun1103.dev@gmail.com>
2021-12-01 23:41:49 +0900

    연습문제2 준비

---
+++ src/com/book/algorithm/stackNque/GenericStack.java
@@ -0,0 +1,29 @@
+package com.book.algorithm.stackNque;
+
+public class GenericStack<E> {
+    private int max;
+    private int pointer;
+    private E[] stack;
+
+    public GenericStack(int max) {
+        this.max = max;
+        pointer = 0;
+        stack = (E[]) new Object[max];
+    }
+
+    // 삽입
+
+    // 삭제
+
+    // 인덱스 조회 (검색)
+
+    // peek
+
+    // size
+
+    // dump
+
+    // 예외 2가지
+
+
+}
--- src/com/book/algorithm/stackNque/IntStackApp.java
+++ src/com/book/algorithm/stackNque/IntStackApp.java
@@ -14,10 +14,89 @@ public class IntStackApp {
         while(true){
             System.out.println("현재 데이터 수: "+stack.size() +" / "+stack.capacity());
 
-            if(br.readLine().equals("GG")){
-                System.out.println("프로그램이 종료하였습니다.");
+            System.out.println("(1) push  (2) pop  (3) peek  (4) dump  (5) indexOf");
+            System.out.println("(6) isEmpty  (7) isFull  (0) End");
+            System.out.print("원하시는 기능을 선택해주세요 >");
+
+            int input = Integer.parseInt(br.readLine());
+
+            if(input == 0){
+                System.out.println("프로그램이 종료되었습니다.");
 
                 break;
+            }
+
+            switch (input){
+                case 1:
+                    System.out.print("데이터: ");
+                    try {
+                        stack.push(Integer.parseInt(br.readLine()));
+
+                    } catch (IntStack.OverflowIntStackException e) {
+                        System.out.println("Stack is full");
+                    }
+                    break;
+
+                case 2:
+                    System.out.print("데이터: ");
+                    try {
+                        stack.push(Integer.parseInt(br.readLine()));
+
+                    } catch (IntStack.EmptyIntStackException e) {
+                        System.out.println("Stack is Empty");
+                    }
+                    break;
+
+                case 3:
+                   try {
+                        System.out.println("peek : "+stack.peek());
+
+                   } catch (IntStack.EmptyIntStackException e) {
+                       System.out.println("Stack is empty");
+                   }
+                   break;
+
+                case 4:
+                    stack.dump();
+
+                   break;
+
+                case 5:
+                    System.out.print("데이터: ");
+                    int index = stack.indexOf(Integer.parseInt(br.readLine()));
+
+                    if(index == -1) {
+                        System.out.println("입력하신 숫자는 스택에 존재하지 않습니다.");
+                    } else {
+                        System.out.println("입력한 값의 stack index > "+index);
+                    }
+
+                    break;
+
+                case 6:
+
+                    if(stack.isEmpty()) {
+                        System.out.println("해당 스택은 비어있습니다.");
+                    } else {
+                        System.out.println("해당 스택은 비어있지 않습니다.");
+                    }
+
+                    break;
+
+                case 7:
+
+                    if(stack.isFull()) {
+                        System.out.println("해당 스택은 가득 차있습니다.");
+                    } else {
+                        System.out.println("해당 스택은 가득 차있지 않습니다.");
+                    }
+
+                    break;
+
+                default:
+                    System.out.println("제시된 숫자 보기에서 선택해 주세요.");
+
+                    break;
             }
         }
     }
