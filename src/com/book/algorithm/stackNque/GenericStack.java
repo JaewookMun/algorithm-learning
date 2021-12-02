6a335a0c800f7e4a8679012c836a00cdf14dbb21
JaewookMun <mun1103.dev@gmail.com>
2021-12-02 23:52:21 +0900

    Q2

--- src/com/book/algorithm/stackNque/GenericStack.java
+++ src/com/book/algorithm/stackNque/GenericStack.java
@@ -12,18 +12,58 @@ public class GenericStack<E> {
     }
 
     // 삽입
+    public E push(E data){
+        if(pointer >= max) throw new RuntimeException("Stack is overflowed");
+
+        stack[pointer++] = data;
+
+        return data;
+    }
 
     // 삭제
+    public E pop() throws RuntimeException {
+        if(pointer <= 0) throw new RuntimeException("Stack is Empty");
+
+        return stack[--pointer];
+    }
 
     // 인덱스 조회 (검색)
+    public int indexOf(E data) {
+        for(int i=pointer-1; i>=0; i--){
+            if(stack[i] == data)
+                return i;
+        }
+
+        return -1;
+    }
 
     // peek
+    public E peek() {
+        if(pointer <= 0) throw new RuntimeException();
+
+        return stack[pointer-1];
+    }
 
     // size
+    public int size() {
+        return pointer;
+    }
 
     // dump
+    public void dump() {
+        for(int i=0; i<pointer; i++){
+            System.out.print(stack[i]);
 
-    // 예외 2가지
+            if(i != pointer-1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+    }
+
+    // OverFlow & Empty Exception...
+    // > cannot..
 
 
 }
---
+++ src/com/book/algorithm/stackNque/GenericStackApp.java
@@ -0,0 +1,9 @@
+package com.book.algorithm.stackNque;
+
+public class GenericStackApp {
+    public static void main(String[] args) {
+        GenericStack<Integer> stackTest = new GenericStack<>(10);
+
+        stackTest.pop();
+    }
+}
