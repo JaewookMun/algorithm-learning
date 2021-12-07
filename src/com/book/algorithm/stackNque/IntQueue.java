406fb66e6d92cc36a4b1b3d158dd6c02e3a16170
JaewookMun <mun1103.dev@gmail.com>
2021-12-07 23:30:31 +0900

    Q5 - 문제풀이 준비

---
+++ src/com/book/algorithm/stackNque/IntQueue.java
@@ -0,0 +1,15 @@
+package com.book.algorithm.stackNque;
+
+// Q5. 검색 메서드 추가 - IntQueue base
+// 링 버퍼(Ring Buffer) - 배열의 처음과 끝이 연결되었다고 보는 자료 구조(프론트와 리어 사용 - 논리적 형태)
+public class IntQueue {
+    private int max;
+    private int size;
+    private int[] que;
+
+    private int front;
+    private int rear;
+
+    // RuntimeException vs Exception -> diff...
+
+}
