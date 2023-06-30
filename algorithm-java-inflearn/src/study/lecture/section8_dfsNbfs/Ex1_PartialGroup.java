81dcff07764de76d8ba9742cfe334309b28e67e2
JaewookMun <mun1103.dev@gmail.com>
2023-06-30 21:17:30 +0900

    Add 합이 같은 부분집합

---
+++ algorithm-java-inflearn/src/study/lecture/section8_dfsNbfs/Ex1_PartialGroup.java
@@ -0,0 +1,38 @@
+package study.lecture.section8_dfsNbfs;
+
+import java.util.Scanner;
+
+// 합이 같은 부분집합 (DFS : 아마존 인터뷰)
+// 일차원 배열의 각 요소 인덱스를 level 처럼 활용하면서 배열을 계속 전달
+public class Ex1_PartialGroup {
+    static String answer = "NO";
+    static int n, total = 0;
+    boolean flag = false;
+
+    public void dfs(int L, int sum, int[] arr) {
+        if(flag) return;
+        if(sum > total/2) return;
+        if(L == n) {
+            if((total-sum) == sum) {
+                answer = "YES";
+                flag = true;
+            }
+        } else {
+            dfs(L+1, sum+arr[L], arr);
+            dfs(L+1, sum, arr);
+        }
+    }
+
+    public static void main(String[] args) {
+        Ex1_PartialGroup T = new Ex1_PartialGroup();
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = in.nextInt();
+            total += arr[i];
+        }
+        T.dfs(0, 0, arr);
+        System.out.println(answer);
+    }
+}
