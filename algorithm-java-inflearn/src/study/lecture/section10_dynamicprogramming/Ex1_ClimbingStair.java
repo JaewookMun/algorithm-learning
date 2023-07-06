cc9ebb9cce6e98ee3693bc87116bab6fa470102f
JaewookMun <mun1103.dev@gmail.com>
2023-07-06 12:49:32 +0900

    1. Add Dynamic programming - ex1

--- algorithm-java-inflearn/src/study/lecture/section10_dynamicprogramming/Ex1_ClimbingStair.java
+++ algorithm-java-inflearn/src/study/lecture/section10_dynamicprogramming/Ex1_ClimbingStair.java
@@ -1,4 +1,28 @@
 package study.lecture.section10_dynamicprogramming;
 
+import java.util.Scanner;
+
+// 계단 오르기
 public class Ex1_ClimbingStair {
+
+    static int[] dy;
+
+    public int solution(int n) {
+        dy[1] = 1;
+        dy[2] = 2;
+        for(int i=3; i<=n; i++) {
+            dy[i] = dy[i-2] + dy[i-1];
+        }
+
+        return dy[n];
+    }
+
+    public static void main(String[] args) {
+        Ex1_ClimbingStair app = new Ex1_ClimbingStair();
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        dy = new int[n + 1];
+        System.out.println(app.solution(n));
+    }
 }
